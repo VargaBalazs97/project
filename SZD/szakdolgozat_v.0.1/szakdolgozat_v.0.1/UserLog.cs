5cea2144f1a1975ac3dd9b09ea024e48b081dc38
Varga Balázs <vbalazsm@gmail.com>
2019-02-15 17:59:02 +0100

    UP

---
+++ SZD/szakdolgozat_v.0.1/szakdolgozat_v.0.1/UserLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace szakdolgozat_v._0._1
+{
+    public partial class UserLog : Form
+    {
+        public UserLog()
+        {
+            InitializeComponent();
+        }
+
+        private void UserLog_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
